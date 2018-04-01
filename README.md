5db5c2c17cc2d025d627683340ea64489b9fb6bf
Andrey Oleinikov <dyt321@ya.ru>
2018-04-01 21:00:39 +0300

    Improved code according to recomendations once again

--- README.md
+++ README.md
@@ -12,12 +12,26 @@
 ```bash
 
 $ python bars.py # possibly requires call of python3 executive instead of just python
-Input file path or "bars.json" is used:
-Biggest bar is: Спорт бар «Красная машина»
-Smallest bar is: БАР. СОКИ
-Input your longitude: 52.11251
-Input your latitude: 31.1214
-Closest bar is: Бар Виват
+Biggest bar is: {'geometry': {'coordinates': [37.638228501070095, 55.70111462948684], 'type': 'Point'}, 'prope
+rties': {'DatasetId': 1796, 'VersionNumber': 2, 'ReleaseNumber': 2, 'RowId': 'fbe6c340-4707-4d74-b7ca-2b84a23b
+f3a8', 'Attributes': {'global_id': 169375059, 'Name': 'Спорт бар «Красная машина»', 'IsNetObject': 'нет', 'Ope
+ratingCompany': None, 'AdmArea': 'Южный административный округ', 'District': 'Даниловский район', 'Address': '
+Автозаводская улица, дом 23, строение 1', 'PublicPhone': [{'PublicPhone': '(905) 795-15-84'}], 'SeatsCount': 4
+50, 'SocialPrivileges': 'нет'}}, 'type': 'Feature'}
+Smallest bar is: {'geometry': {'coordinates': [37.35805920566864, 55.84614475898795], 'type': 'Point'}, 'prope
+rties': {'DatasetId': 1796, 'VersionNumber': 2, 'ReleaseNumber': 2, 'RowId': '17adc22c-5c41-4e4b-872f-815b521f
+2b53', 'Attributes': {'global_id': 20675518, 'Name': 'БАР. СОКИ', 'IsNetObject': 'нет', 'OperatingCompany': No
+ne, 'AdmArea': 'Северо-Западный административный округ', 'District': 'район Митино', 'Address': 'Дубравная ули
+ца, дом 34/29', 'PublicPhone': [{'PublicPhone': '(495) 258-94-19'}], 'SeatsCount': 0, 'SocialPrivileges': 'нет
+'}}, 'type': 'Feature'}
+Input your longitude: 5
+Input your latitude: 4
+Closest bar is: {'geometry': {'coordinates': [36.900000000253, 55.303299999814], 'type': 'Point'}, 'properties
+': {'DatasetId': 1796, 'VersionNumber': 2, 'ReleaseNumber': 2, 'RowId': 'bb9eb30d-d16b-4821-8d9c-894b581ac762'
+, 'Attributes': {'global_id': 281494712, 'Name': 'Staropramen', 'IsNetObject': 'нет', 'OperatingCompany': None
+, 'AdmArea': 'Центральный административный округ', 'District': 'Красносельский район', 'Address': 'Садовая-Спа
+сская улица, дом 19, корпус 1', 'PublicPhone': [{'PublicPhone': '(985) 069-34-47'}], 'SeatsCount': 50, 'Social
+Privileges': 'нет'}}, 'type': 'Feature'}
 
 ```
 
--- bars.py
+++ bars.py
@@ -1,5 +1,6 @@
 import json
 import os
+import sys
 
 
 def load_data(filepath):
@@ -9,64 +10,58 @@ def load_data(filepath):
     return False
 
 
-def get_biggest_bar(bars_json):
-    bars_list = []
-    for feature in bars_json['features']:
-        bar_name = feature['properties']['Attributes']['Name']
-        seats_count = feature['properties']['Attributes']['SeatsCount']
-        bars_list.append((bar_name, seats_count))
-    biggest_bar = max(bars_list, key=lambda item: item[1])
-    return biggest_bar[0]
+def get_biggest_bar(bars_list):
+    biggest_bar = max(bars_list, key=lambda item:
+                      item['properties']['Attributes']['SeatsCount'])
+    return biggest_bar
 
 
-def get_smallest_bar(bars_json):
-    bars_list = []
-    for feature in bars_json['features']:
-        bar_name = feature['properties']['Attributes']['Name']
-        seats_count = feature['properties']['Attributes']['SeatsCount']
-        bars_list.append((bar_name, seats_count))
-    smallest_bar = min(bars_list, key=lambda item: item[1])
-    return smallest_bar[0]
+def get_smallest_bar(bars_list):
+    smallest_bar = min(bars_list, key=lambda item:
+                       item['properties']['Attributes']['SeatsCount'])
+    return smallest_bar
 
 
-def get_closest_bar(bars_json, longitude, latitude):
-    bars_list = []
-    for feature in bars_json['features']:
-        bar_name = feature['properties']['Attributes']['Name']
-        bar_longitude, bar_latitude = feature['geometry']['coordinates']
-        distance = ((longitude - float(bar_longitude))**2 +
-                    (latitude - float(bar_latitude))**2)**(1 / 2)
-        bars_list.append((bar_name, distance))
-    closest_bar = min(bars_list, key=lambda item: item[1])
-    return closest_bar[0]
+def get_closest_bar(bars_list, longitude, latitude):
+    biggest_distance = 40000
+    closest_bar = ''
+    for bar in bars_list:
+        distance = ((longitude - bar['geometry']['coordinates'][0])**2 +
+                    (latitude - bar['geometry']['coordinates'][1])**2)**(1 / 2)
+        if distance < biggest_distance:
+            biggest_distance = distance
+            closest_bar = bar
+    return closest_bar
 
 
 def coordinate_to_float(coordinate_str):
     try:
         coordinate = float(coordinate_str)
         return coordinate
-    except ValueError as e:
+    except ValueError:
         return False
 
 
-def process_bars(bars_json):
-    print('Biggest bar is:', get_biggest_bar(bars_json))
-    print('Smallest bar is:', get_smallest_bar(bars_json))
-
-    longitude = coordinate_to_float(input('Input your longitude: '))
-    latitude = coordinate_to_float(input('Input your latitude: '))
-
-    if longitude and latitude:
-        print('Closest bar is:',
-              get_closest_bar(bars_json, longitude, latitude))
-    else:
-        print('Wrong coordinates')
-
-
 if __name__ == '__main__':
-    filepath = input('Input file path or "bars.json" is used: ')
-    bars_json = load_data(filepath) or load_data('bars.json')
-    if bars_json:
-        process_bars(bars_json)
+    filepath = 'bars.json'
+    if len(sys.argv) > 1:
+        filepath = sys.argv[1]
+
+    bars_list = load_data(filepath)['features']
+    if bars_list:
+
+        print('Biggest bar is:', get_biggest_bar(bars_list))
+        print('Smallest bar is:', get_smallest_bar(bars_list))
+
+        longitude = coordinate_to_float(input('Input your longitude: '))
+        latitude = coordinate_to_float(input('Input your latitude: '))
+
+        if longitude and latitude:
+            print('Closest bar is:',
+                  get_closest_bar(bars_list, longitude, latitude))
+
+        else:
+            print('Wrong coordinates')
+
     else:
         print('Wrong file or path')
